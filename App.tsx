30505efe95483d0e701c71c3c0ddb75ee37be34c
TienPhuc02 <dtp2352002@gmail.com>
2023-12-05 16:44:17 +0700

    set up nativewind ts with react native cli

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text className="text-red-500">phuc react-native dev</Text>
+    </View>
+  );
+};
+
+export default App;
